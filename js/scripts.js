aa7c7f36ecac32687b138385827b8eb5a4d65d7c
Aaron Whang <tlbhibiki@gmail.com>
2019-06-14 10:29:30 -0700

    it will create the input form and comfirm that the input and button works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
+//takes user input number by form submit button
+//fill an array with numbers up to the user's inputted numbers
+//outputs that array in as a list on the web page
+//write a loop that will check every value for 3 2 and 1 in that priority
+//will replace values above as specified, 3 becomes "I'm sorry....", 2 becomes "Boop!", 1 becomes "Beep!"
+//write an output that will reset on every form submit
+//extra features:
+//will return a message if not an number or a negative number
+//will pop out little hal images, increasing by one every time it runs past a 3 in the output
+//make a new button that will reverse the output from highest to lowest
+//add a name form that will replace "Dave" in the 3 condition
+
 $(document).ready(function(){
+  $("form#beep").submit(function(event) {
+    event.preventDefault();
+    var romanVar = parseInt($("input#boop").val());
+    alert(romanVar);
 
 
 
@@ -6,6 +22,5 @@ $(document).ready(function(){
 
 
 
-
-
+ });
 });
